4b903715aafc157fa576a23b0223d678712a7567
KrykiZZ <krykizzzz@gmail.com>
2024-08-17 03:30:24 +0400

    autogenerated types

---
+++ types/pathing.d.ts
@@ -0,0 +1,64 @@
+export = Pathing;
+declare const Pathing: PathingManager;
+declare class PathingManager {
+    constructor(options?: {});
+    onRoomEnter: any;
+    getCreepWorkingTarget: any;
+    getCreepInstance: any;
+    getCreepEntity: any;
+    avoidRooms: any;
+    matrixCache: any;
+    roomMoves: any;
+    lastMoveTime: any;
+    clearMatrixCache(): void;
+    clearMatrixCacheRoom(roomName: any): void;
+    moveTo(creep: any, target: any, defaultOptions?: {}): any;
+    moveOffRoad(creep: any, options?: {}): any;
+    getMoveDirection(creep: any): any;
+    getCreepPath(creep: any): any;
+    deserializeMove(data: any): any[];
+    serializeMove(targetPos: any, creepPos: any, path: any): any[];
+    visualizePath(startPos: any, path: any, targetPos: any, range: any, style: any): void;
+    insertMove(roomName: any, move: any): void;
+    removeMove(moves: any, creepName: any): void;
+    increaseMovePriority(moves: any, fromIndex: any, creepName: any, priority: any): void;
+    getMoves(roomName: any): any;
+    reservePos(pos: any, priority: any): any;
+    hasMove(pos: any, moves: any, priority: any): any;
+    hasMoveWithOffRoad(pos: any, moves: any, priority: any): any;
+    runMoves(): void;
+    runMovesRoom(roomName: any): void;
+    cleanup(): void;
+    getCreepTargetInfo(creep: any, roomName: any): any;
+    isCreepCanMove(creep: any): any;
+    moveCreeps(moves: any): void;
+    getObstacleCreep(pos: any, room: any): any;
+    hasObstacleHostileCreep(room: any, x: any, y: any): any;
+    hasObstacleCreep(room: any, x: any, y: any): any;
+    getAdjacentPositions(pos: any): {};
+    getCreepMovePos(creep: any, priority: any, moves: any, pathEnd: any): any;
+    getCreepPushPos(creep: any, preferOffRoad: any, priority: any, moves: any, targetInfo: any): any;
+    getCreepOffRoadMovePos(creep: any, offRoad: any, priority: any, moves: any, targetInfo: any): any;
+    addWorkingCreepsToMatrix(matrix: any, room: any, priority: any): void;
+    findPath(startPos: any, targetPos: any, defaultOptions?: {}): any;
+    findRoute(startPos: any, targetPos: any, options?: {}): any;
+    getRoomDistance(startPos: any, targetPos: any): number;
+    getTargetRangePositions(targetPos: any, range: any, options: any): {
+        pos: {
+            x: number;
+            y: any;
+            roomName: any;
+        } | {
+            x: any;
+            y: number;
+            roomName: any;
+        };
+        range: number;
+    }[];
+    fixPath(path: any, matrix: any, roomName: any): void;
+    serializePath(startPos: any, path: any): any[];
+    getPathEnd(startPos: any, path: any): any;
+    getCostMatrix(roomName: any, options?: {}, matrixKey?: any): any;
+    addHostilesToMatrix(matrix: any, pos: any, room: any, range: any): void;
+    makeStructuresMatrix(room: any, options: any): any;
+}
---
+++ types/pathing.utils.d.ts
@@ -0,0 +1,17 @@
+export function hasActiveBodyparts(creep: any, partType: any): boolean;
+export function logError(error: any): void;
+export function getRange(pos1: any, pos2: any): number;
+export function posToCoords(pos: any): {
+    x: any;
+    y: any;
+};
+export function offsetPosCoords(pos: any, direction: any): {
+    x: any;
+    y: any;
+};
+export function offsetPos(pos: any, direction: any): any;
+export function isPosEqual(pos1: any, pos2: any): boolean;
+export function getDirection(pos1: any, pos2: any): any;
+export function isPosExit(pos: any): boolean;
+export function isCoordsEqual(pos1: any, pos2: any): boolean;
+export function lookInRange(pos: any, room: any, look: any, range: any): any;
